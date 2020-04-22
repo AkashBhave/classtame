68d07b462c1b6f994057aa53ba262480dadc2950
Akash Bhave <akashbbhave@gmail.com>
2020-04-22 11:30:47 -0400

    Setup basic CLI struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "classtame", about = "Manage classes with ease.")]
+struct Opt {
+    #[structopt(name = "command")]
+    command: String,
+    #[structopt(required_if("command", "info"))]
+    class: Option<String>,
+    #[structopt(
+        default_value = "~/.classtame.yaml",
+        short = "c",
+        long,
+        parse(from_os_str)
+    )]
+    config: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Opt::from_args();
 }
