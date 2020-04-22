b39761962caad6f42322889e62c2a561ade06977
Akash Bhave <akashbbhave@gmail.com>
2020-04-22 12:05:34 -0400

    Convert CLI options struct into enum

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,13 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "classtame", about = "Manage classes with ease.")]
-struct Opt {
-    #[structopt(name = "command")]
-    command: String,
-    #[structopt(required_if("command", "info"))]
-    class: Option<String>,
-    #[structopt(
-        default_value = "~/.classtame.yaml",
-        short = "c",
-        long,
-        parse(from_os_str)
-    )]
-    config: PathBuf,
+enum Opt {
+    Info {},
+    Open {},
+    Mod {},
 }
 
 fn main() {
     let args = Opt::from_args();
+    println!("{:?}", &args)
 }
