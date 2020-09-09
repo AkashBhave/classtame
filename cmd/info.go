ee88f69fe833dea553b0b53f2e3fa89a63a0eaee
Akash Bhave <akashbbhave@gmail.com>
2020-09-08 23:42:07 -0400

    Develop 'info' command

--- cmd/info.go
+++ cmd/info.go
@@ -1,30 +1,46 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
+	"akashbhave.com/classtame/util"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // infoCmd represents the info command
 var infoCmd = &cobra.Command{
 	Use:   "info",
-	Short: "Return information about a class",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("info called")
+	Short: "Return information about a class", Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Class doesn't exist
+		if !viper.IsSet(fmt.Sprintf("classes.%s", args[0])) {
+			return errors.New("Class doesn't exist, use 'new'")
+		}
+
+		var class util.Class
+		viper.UnmarshalKey(fmt.Sprintf("classes.%s", args[0]), &class)
+
+		if class.Name != "" {
+			fmt.Printf("Name: %s\n", class.Name)
+		}
+		fmt.Printf("Alias: %s\n", args[0])
+		if class.Period != 0 {
+			fmt.Printf("Period: %d\n", class.Period)
+		}
+		if class.Link != "" {
+			fmt.Printf("Link: %s\n", class.Link)
+		}
+		if class.Teacher != "" {
+			fmt.Printf("Teacher: %s\n", class.Teacher)
+		}
+
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(infoCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// infoCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// infoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
